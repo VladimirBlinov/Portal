e1f806ca11db909799b2a3e681957468032c8115
VladimirBlinov <b.b.blinov@yandex.ru>
2021-12-24 23:11:25 +0300

    + instrument_uploader_daily_dag

---
+++ invportal/docker/airflow/dags/instruments_uploader_daily.py
@@ -0,0 +1,77 @@
+from datetime import datetime
+import logging
+import os
+import pandas as pd
+
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.python import PythonOperator
+from airflow.hooks.postgres_hook import PostgresHook
+
+from etlutils.insrtuments_helper import get_instruments, get_extracted, get_df_from_db, compare_df
+
+DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'start_date': datetime(2021, 12, 15),
+}
+
+DAG_ID = 'instrument_uploader_daily_dag'
+TAG = 'instrument_uploader_daily'
+TEMP_DIR = '/tmp'
+WORK_DIR = os.path.join(TEMP_DIR, DAG_ID)
+if not os.path.exists(WORK_DIR):
+    os.mkdir(WORK_DIR)
+EXTRACTED_FILE_PATH = os.path.join(WORK_DIR, f'extracted_{DAG_ID}.csv')
+TRANSFORMED_FILE_PATH = os.path.join(WORK_DIR, f'transformed_{DAG_ID}.csv')
+
+with DAG(
+        DAG_ID,
+        default_args=DEFAULT_ARGS,
+        schedule_interval='@daily',
+        start_date=datetime(2021, 12, 18),
+        catchup=False,
+        tags=[TAG]) as dag:
+    def extract():
+        logging.info(EXTRACTED_FILE_PATH)
+        instruments = get_instruments()
+        logging.info(EXTRACTED_FILE_PATH)
+        instruments.to_csv(EXTRACTED_FILE_PATH, index_label=False, header=False, sep=';')
+
+
+    extract_task = PythonOperator(
+        task_id='extract_task',
+        python_callable=extract,
+    )
+
+
+    def transform():
+        pg_hook = PostgresHook('airflow_database')
+        reference_df = pd.DataFrame()
+        extracted_df = get_extracted(EXTRACTED_FILE_PATH)
+        with pg_hook.get_conn() as conn:
+            with conn.cursor() as cursor:
+                try:
+                    cursor.execute("""SELECT "InstrumentID", "Instrument", "Ticker", "MarketplaceID"
+                                FROM public."Instrument";""")
+                    for row in cursor:
+                        reference_df = reference_df.append(pd.Series(row), ignore_index=True)
+                    transformed_df = compare_df(reference_df.iloc[3:, :], extracted_df)
+                    transformed_df.to_csv(TRANSFORMED_FILE_PATH, index_label=False, header=False, sep=';')
+                except Exception as e:
+                    logging.info('Exception:', e)
+
+
+    transform_task = PythonOperator(
+        task_id="transform_task",
+        python_callable=transform
+    )
+
+    load_task = PostgresOperator(
+        task_id='load_task',
+        postgres_conn_id='airflow_database',
+        sql=f"""COPY public."Instrument" FROM '{EXTRACTED_FILE_PATH}' DELIMITER ';'
+            """
+    )
+
+    extract_task >> transform_task >> load_task
+    # extract_task >> load_task >> pg_load
---
+++ invportal/docker/airflow/plugins/etlutils/insrtuments_helper.py
@@ -0,0 +1,80 @@
+from finam import Exporter
+import json
+import logging
+import os
+import psycopg2
+from dotenv import load_dotenv
+import pandas as pd
+
+
+load_dotenv(r'D:\Django\portal\invportal\docker\airflow\database.env')
+file = r'\\wsl$\docker-desktop-data\version-pack-data\community\docker\volumes\airflow_tmp\_data\instruments.csv'
+
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+LOCAL_POSTGRES_HOST = os.getenv('LOCAL_POSTGRES_HOST')
+LOCAL_POSTGRES_PORT = os.getenv('LOCAL_POSTGRES_PORT')
+
+
+def get_instruments(market=25):
+    exporter = Exporter()
+    market_instruments = exporter.lookup(market=market)
+    market_instruments.to_csv('instr.csv', index_label=False, header=False, sep=';')
+    return market_instruments
+
+
+def connect_db(query, dbname=POSTGRES_DB, user_name=POSTGRES_USER, pwd=POSTGRES_PASSWORD, host=LOCAL_POSTGRES_HOST,
+               port=LOCAL_POSTGRES_PORT):
+    conn = psycopg2.connect(dbname=dbname, user=user_name,
+                            password=pwd, host=host, port=port)
+    cursor = conn.cursor()
+    try:
+        cursor.execute(query)
+        return cursor
+    except Exception as e:
+        print('Exception:', e)
+        cursor.close()
+        conn.close()
+        return None
+
+
+def get_df_from_db(query, dbname=POSTGRES_DB, user_name=POSTGRES_USER, pwd=POSTGRES_PASSWORD,
+                         host=LOCAL_POSTGRES_HOST, port=LOCAL_POSTGRES_PORT):
+    conn = psycopg2.connect(dbname=dbname, user=user_name,
+                            password=pwd, host=host, port=port)
+    try:
+        df = pd.read_sql(query, con=conn)
+        return df
+    except Exception as e:
+        print('Exception:', e)
+        conn.close()
+        return None
+
+def get_extracted(file_path):
+    df = pd.read_csv(file_path, sep=';', header=None)
+    return df
+
+
+def compare_df(ref_df, new_df):
+    new_df.columns = ref_df.columns.to_list()
+    set_ref = set(ref_df.iloc[:, 2])
+    set_new = set(new_df.iloc[:, 2])
+    set_diff = set_new - set_ref
+    diff_df = new_df[new_df.iloc[:, 2].isin(set_diff)]
+    return diff_df
+
+
+if __name__ == '__main__':
+    query = """SELECT "InstrumentID", "Instrument", "Ticker", "MarketplaceID"
+                                FROM public."Instrument";"""
+    extracted_df = get_extracted(file)
+    # reference_df = get_df_from_db(query)
+    reference_df = pd.DataFrame()
+    cursor = connect_db(query)
+    for row in cursor:
+        reference_df = reference_df.append(pd.Series(row), ignore_index=True)
+    transformed_df = compare_df(reference_df.iloc[3:, :], extracted_df)
+    print(transformed_df)
+
+
